8104c07fab03ff2cec873e597d56ecaef336b0a2
Ismayilgit24 <ismayilibrahinzad9@gmail.com>
2024-10-25 12:46:18 +0400

    Initial commit

---
+++ Generic Type, Generic Collections/Generic Type, Generic Collections/ListInt.cs
@@ -0,0 +1,115 @@
+﻿public class ListInt<T>
+{
+	private T[] _arr;
+	private int _count;
+	private int _capacity;
+
+	public ListInt()
+	{
+		_capacity = 4;
+		_arr = new T[_capacity];
+		_count = 0;
+	}
+
+	public int Count
+	{
+		get { return _count; }
+	}
+
+	public int Capacity
+	{
+		get { return _capacity; }
+	}
+
+	public T this[int index]
+	{
+		get
+		{
+			if (index >= 0 && index < _count)
+			{
+				return _arr[index];
+			}
+			throw new IndexOutOfRangeException("Index düzgün deyil!");
+		}
+		set
+		{
+			if (index >= 0 && index < _count)
+			{
+				_arr[index] = value;
+			}
+			else
+			{
+				throw new IndexOutOfRangeException("Index düzgün deyil!");
+			}
+		}
+	}
+
+	public void Add(T item)
+	{
+		if (_count == _capacity)
+		{
+			_capacity *= 2;
+			Array.Resize(ref _arr, _capacity);
+		}
+		_arr[_count] = item;
+		_count++;
+	}
+
+	public void AddRange(params T[] items)
+	{
+		if (_count + items.Length > _capacity)
+		{
+			while (_capacity < _count + items.Length)
+			{
+				_capacity *= 2;
+			}
+			Array.Resize(ref _arr, _capacity);
+		}
+		for (int i = 0; i < items.Length; i++)
+		{
+			_arr[_count] = items[i];
+			_count++;
+		}
+	}
+
+	
+	public bool Contains(T item)
+	{
+		for (int i = 0; i < _count; i++)
+		{
+			if (_arr[i].Equals(item))
+			{
+                
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void Remove(T item)
+	{
+		int index = Array.IndexOf(_arr, item, 0, _count);
+		if (index >= 0)
+		{
+			for (int i = index; i < _count - 1; i++)
+			{
+				_arr[i] = _arr[i + 1];
+			}
+			_count--;
+		}
+	}
+
+	public void RemoveRange(params T[] items)
+	{
+		foreach (var item in items)
+		{
+			Remove(item);
+		}
+	}
+
+	public override string ToString()
+	{
+        
+		return string.Join(", ", _arr.Take(_count));
+	}
+}
---
+++ Generic Type, Generic Collections/Generic Type, Generic Collections/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Generic_Type__Generic_Collections
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			ListInt<int> listint = new ListInt<int>();
+			listint.Add(5);
+			listint.Add(24);
+			listint.Add(63);
+			listint.Add(1);
+			listint.Add(7);
+
+			Console.WriteLine($"Added elements : {listint}");
+
+			listint.Remove(5);
+			Console.WriteLine($"Removed elment(s) : {listint}");
+
+			listint.AddRange(1, 2, 3, 4, 5);
+			Console.WriteLine($"Added ranges : {listint}");
+
+			Console.WriteLine(listint.Contains(5));
+
+			listint.RemoveRange(1, 1, 3);
+			Console.WriteLine($"Removed range : {listint}");
+
+			listint.ToString();
+		}
+	}
+}
+
+
